ff85eca6aa9866a5eeec0650d22f3c0079a0b306
Jenish Budhathoki <Budathokizone111@gmail.com>
2025-01-08 13:05:31 +0545

    reached added erorr handling and started backend

---
+++ Backend/server.ts
@@ -0,0 +1,10 @@
+import {app} from "./app";
+import connectDB from "./utils/db";
+require("dotenv").config();
+
+
+// create server
+app.listen(process.env.PORT, () => {
+    console.log(`Sever is connected with port ${process.env.PORT}`);
+    connectDB();
+});
